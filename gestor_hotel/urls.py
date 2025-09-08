991b0c30a64cc4de6b80824ca82be25a57362d88
franciscoVza <franvasquezalcantara@gmail.com>
2025-09-08 12:14:55 -0300

    arreglo error al cerrar sesion

--- gestor_hotel/urls.py
+++ gestor_hotel/urls.py
@@ -19,20 +19,21 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from hotel import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    # URLs principales
     path('', views.inicio, name='inicio'),
     path('habitaciones/', views.lista_habitaciones, name='lista_habitaciones'),
     path('reservar/<int:habitacion_id>/', views.hacer_reserva, name='hacer_reserva'),
     path('mis-reservas/', views.mis_reservas, name='mis_reservas'),
     path('registrarse/', views.registrarse, name='registrarse'),
 
-    # URLs de administración (estas faltaban!)
+
     path('admin/reservas/', views.gestionar_reservas, name='gestionar_reservas'),
     path('admin/reserva/<int:reserva_id>/cambiar-estado/', views.cambiar_estado_reserva, name='cambiar_estado_reserva'),
     path('admin/habitacion/<int:habitacion_id>/cambiar-estado/', views.cambiar_estado_habitacion, name='cambiar_estado_habitacion'),
     path('admin/agregar-habitacion/', views.agregar_habitacion, name='agregar_habitacion'),
     path('accounts/login/', auth_views.LoginView.as_view(), name='iniciar_sesion'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='cerrar_sesion'),
+    path("accounts/logout/", LogoutView.as_view, name="logout"),
 ]
